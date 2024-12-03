7d5317b19782566dab916d1f96271959db9a2726
singamy007 <112240460+singamy007@users.noreply.github.com>
2024-12-03 11:14:48 +0530

    Add files via upload

--- evaluation .js
+++ evaluation .js
@@ -1,9 +1,9 @@
-function findMissingNumber(arr) {
-    const n = arr.length + 1; // Array size + 1
-    const totalSum = (n * (n + 1)) / 2; // Sum of first n natural numbers
-    const arraySum = arr.reduce((acc, x) => acc + x, 0);
-    return totalSum - arraySum;
+function add(a) {
+    return function(b) {
+      return a + b;
+    };
   }
   
-  console.log(findMissingNumber([1, 2, 3, 5, 6])); // Output: 4
+  const add5 = add(5); // Returns a function
+  console.log(add5(3)); // Output: 8
   
