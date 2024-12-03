a556fe94db8e18ae7f6e884b2ac5328d8df3ba6d
singamy007 <112240460+singamy007@users.noreply.github.com>
2024-12-03 10:43:55 +0530

    Add files via upload

---
+++ evaluation .js
@@ -0,0 +1,23 @@
+class Animal {
+    constructor(name) {
+      this.name = name;
+    }
+    speak() {
+      console.log(`${this.name} makes a sound.`);
+    }
+  }
+  
+  class Dog extends Animal {
+    constructor(name, breed) {
+      super(name); // Calls the parent class's constructor
+      this.breed = breed;
+    }
+    speak() {
+      super.speak(); // Calls the parent class's speak method
+      console.log(`${this.name} barks.`);
+    }
+  }
+  
+  const dog = new Dog("Max", "Golden Retriever");
+  dog.speak();
+  
