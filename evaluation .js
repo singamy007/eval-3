5c32ee119cdf75c557fd2c69be4ea2a528d6d790
singamy007 <112240460+singamy007@users.noreply.github.com>
2024-12-03 10:51:05 +0530

    Add files via upload

--- evaluation .js
+++ evaluation .js
@@ -1,23 +1,22 @@
-class Animal {
-    constructor(name) {
-      this.name = name;
+function isPalindrome(word) {
+    let length = word.length;
+  
+    // Compare characters from the beginning and end of the string
+    for (let i = 0; i < length / 2; i++) {
+      if (word[i] !== word[length - 1 - i]) {
+        return false; // If characters don't match, it's not a palindrome
+      }
     }
-    speak() {
-      console.log(`${this.name} makes a sound.`);
-    }
+    
+    return true; // If all characters match, it's a palindrome
   }
   
-  class Dog extends Animal {
-    constructor(name, breed) {
-      super(name); // Calls the parent class's constructor
-      this.breed = breed;
-    }
-    speak() {
-      super.speak(); // Calls the parent class's speak method
-      console.log(`${this.name} barks.`);
-    }
-  }
+  // Input from user
+  const word = prompt("Enter a word to check if it's a palindrome:");
   
-  const dog = new Dog("Max", "Golden Retriever");
-  dog.speak();
+  if (isPalindrome(word)) {
+    console.log(`${word} is a palindrome.`);
+  } else {
+    console.log(`${word} is not a palindrome.`);
+  }
   
